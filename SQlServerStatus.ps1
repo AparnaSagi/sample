97f1c310cc4917356d4e73d39335e2568c3d31b3
AparnaSagi <73836252+AparnaSagi@users.noreply.github.com>
2021-01-22 21:29:38 +0530

    Update SQlServerStatus.ps1

--- SQlServerStatus.ps1
+++ SQlServerStatus.ps1
@@ -273,8 +273,8 @@ write-host $frag4
 
 ConvertTo-HTML -head $a -PostContent $frag1,$frag2,$frag3,$frag4 -PreContent '<h1 style="color:#2F4F4F"><center><U>SQL Server Heatlh Check Report</U></center></h1>'| Out-File $OutputFile_new
 
-$fromaddress = "Aparna.Sagi@teachforamerica.org" 
-$toaddress = "Aparna.Sagi@teachforamerica.org" 
+$fromaddress = "satish.jagarlamudi@teachforamerica.org" 
+$toaddress = "satish.jagarlamudi@teachforamerica.org" 
 $Subject = "SQL Server Status Report($dataSource)" 
 $body = Get-Content $OutputFile_new
 #$attachment = "D:\UWDBRSTR01.html" 
